9388e02f92b9b4fb282f75cdf16c868a16629cd0
Kohei Hayakawa <hayakawa.kohei.rk@chabanaworks.com>
2021-01-07 13:02:32 +0900

    ファイルサーバー実装

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,19 @@
-import http, {IncomingMessage, ServerResponse} from 'http'
+import http, { IncomingMessage, ServerResponse } from 'http'
 import * as fs from 'fs'
 
 const port = 8888
 
 const server = http.createServer((req: IncomingMessage, res: ServerResponse) => {
-    fs.readFile('./statics/index.html', (err, data)=>{
-        if(err){
-            res.writeHead(500, {'Content-Type':'text/plain'})
+    const path = req.url
+    console.log(path)
+
+    fs.readFile('./statics' + path, (err, data) => {
+        if (err) {
+            res.writeHead(500, { 'Content-Type': 'text/plain' })
             res.write('500 Server Error.')
             res.end()
-        }else{
-            res.writeHead(200, {'Content-Type':'text/html'})
+        } else {
+            res.writeHead(200, { 'Content-Type': 'text/html' })
             res.write(data)
             res.end()
         }
