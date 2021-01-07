af18ea786bc157211def62b995ef106f03429eb4
Kohei Hayakawa <hayakawa.kohei.rk@chabanaworks.com>
2021-01-07 12:14:24 +0900

    readFileで実装

--- src/index.ts
+++ src/index.ts
@@ -4,28 +4,16 @@ import * as fs from 'fs'
 const port = 8888
 
 const server = http.createServer((req: IncomingMessage, res: ServerResponse) => {
-    const rs = fs.createReadStream('./statics/index.html', {encoding: 'utf-8'})
-
-    // rs.pipe(res)
-
-    rs.on("open", (chunk)=>{
-        res.writeHead(200, {'Content-Type':'text/html'})
-    })
-
-    rs.on("data", (chunk)=>{
-        res.write(chunk)
-    })
-
-    rs.on("end", ()=>{
-        console.log('file loaded.')
-        res.end()
-    })
-
-    rs.on("error", (err)=>{
-        res.writeHead(500, 'Server Internal Error.', {'Content-Type':'text/plain'})
-        console.error(err)
-        res.write('500 Server Internal Error.\n')
-        res.end()
+    fs.readFile('./statics/index.html', (err, data)=>{
+        if(err){
+            res.writeHead(500, {'Content-Type':'text/plain'})
+            res.write('500 Server Error.')
+            res.end()
+        }else{
+            res.writeHead(200, {'Content-Type':'text/html'})
+            res.write(data)
+            res.end()
+        }
     })
 })
 
