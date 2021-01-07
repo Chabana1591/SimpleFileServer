8f6b62c365136d0014cc1bfbc626c475d7c1ea71
Kohei Hayakawa <hayakawa.kohei.rk@chabanaworks.com>
2021-01-07 16:00:23 +0900

    Content-Typeの判定を追加

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,46 @@
 import http, { IncomingMessage, ServerResponse } from 'http'
 import * as fs from 'fs'
+import mime from 'mime-types'
 
 const port = 8888
 
 const server = http.createServer((req: IncomingMessage, res: ServerResponse) => {
-    
+
     const path: string = toFilePath(req.url)
     console.log(`${req.url}(${path}にフォワード)`)
 
     fs.readFile('./statics' + path, (err, data) => {
         if (err) {
+            console.log(err)
             res.writeHead(500, { 'Content-Type': 'text/plain' })
             res.write('500 Server Error.')
+            res.end()
         } else {
-            res.writeHead(200, { 'Content-Type': 'text/html' })
+            const mimeType: string = toMimeStr(mime.lookup(path))
+            console.log('mimType: ' + mimeType)
+            res.writeHead(200, { 'Content-Type': mimeType })
             res.write(data)
+            res.end()
         }
-        res.end()
     })
 })
 
 server.listen(port)
 console.log(`Server has started (PORT:${port.toString()}).`)
 
-function toFilePath(url:string|undefined):string{
-    if(typeof url === 'undefined'){
+function toFilePath(url: string | undefined): string {
+    if (typeof url === 'undefined') {
         return ''
     }
-    if(url.match(/^\/+$/)){
+    if (url.match(/^\/+$/)) {
         return '/index.html'
     }
     return url.normalize().replace(/^(\.\.(\/|\\|$))+/, '')
+}
+
+function toMimeStr(mimeType: string | false): string {
+    if (typeof mimeType === 'boolean') {
+        return 'text/plain'
+    }
+    return mimeType
 }
