02f6819c818d611b59fcb639fa42bf2833b50fec
Kohei Hayakawa <hayakawa.kohei.rk@chabanaworks.com>
2021-01-07 13:20:58 +0900

    index.htmlをホームに指定

--- src/index.ts
+++ src/index.ts
@@ -4,21 +4,31 @@ import * as fs from 'fs'
 const port = 8888
 
 const server = http.createServer((req: IncomingMessage, res: ServerResponse) => {
-    const path = req.url
-    console.log(path)
+    
+    const path: string = toFilePath(req.url)
+    console.log(req.url)
 
     fs.readFile('./statics' + path, (err, data) => {
         if (err) {
             res.writeHead(500, { 'Content-Type': 'text/plain' })
             res.write('500 Server Error.')
-            res.end()
         } else {
             res.writeHead(200, { 'Content-Type': 'text/html' })
             res.write(data)
-            res.end()
         }
+        res.end()
     })
 })
 
 server.listen(port)
 console.log(`Server has started (PORT:${port.toString()}).`)
+
+function toFilePath(url:string|undefined):string{
+    if(typeof url === 'undefined'){
+        return ''
+    }
+    if(url.match(/^\/+$/)){
+        return '/index.html'
+    }
+    return url.normalize().replace(/^(\.\.(\/|\\|$))+/, '')
+}
