fbe0420c96b4cb0bfbff7812b3178e2138cd8ed4
Kohei Hayakawa <hayakawa.kohei.rk@chabanaworks.com>
2021-01-07 11:54:53 +0900

    createReadStreamで実装

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,32 @@
 import http, {IncomingMessage, ServerResponse} from 'http'
+import * as fs from 'fs'
 
 const port = 8888
 
 const server = http.createServer((req: IncomingMessage, res: ServerResponse) => {
-    res.writeHead(200, 'OK', {'Content-Type':'text/plain'})
-    res.write('Hello, world!\n')
-    res.end()
+    const rs = fs.createReadStream('./statics/index.html', {encoding: 'utf-8'})
+
+    // rs.pipe(res)
+
+    rs.on("open", (chunk)=>{
+        res.writeHead(200, {'Content-Type':'text/html'})
+    })
+
+    rs.on("data", (chunk)=>{
+        res.write(chunk)
+    })
+
+    rs.on("end", ()=>{
+        console.log('file loaded.')
+        res.end()
+    })
+
+    rs.on("error", (err)=>{
+        res.writeHead(500, 'Server Internal Error.', {'Content-Type':'text/plain'})
+        console.error(err)
+        res.write('500 Server Internal Error.\n')
+        res.end()
+    })
 })
 
 server.listen(port)
